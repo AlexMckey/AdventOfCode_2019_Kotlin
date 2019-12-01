6885aef0ef3703db4d11a90b9d7affeff208a4ab
Alex Mc'key <mckey2002@yandex.ru>
2019-12-01 10:07:37 +0400

    Solution_01 - part2 - Success

--- src/Sol_01.kt
+++ src/Sol_01.kt
@@ -2,10 +2,23 @@ import java.io.BufferedReader
 import java.io.File
 import kotlin.streams.toList
 
+fun calcFuel(mass: Int): Int {
+    return Math.floorDiv(mass, 3) - 2
+}
+
+fun calcRecFuel(mass: Int, acc: Int = 0): Int {
+    val newMass = calcFuel(mass)
+    return if (newMass > 0) {
+        calcRecFuel(newMass, acc + newMass)
+    } else acc
+}
+
 fun main() {
     val fileName = "out/production/KtAOC2019/input01.txt"
     val inputStream: BufferedReader = File(fileName).bufferedReader()
     val masses = inputStream.lines().map{it.toInt()}.toList()
-    val res1 = masses.map { Math.floorDiv(it, 3) - 2 }.sum()
+    val res1 = masses.map { calcFuel(it) }.sum()
     println(res1) //3342351
+    val res2 = masses.map { calcRecFuel(it) }.sum()
+    println(res2) //
 }
--- src/sol_01.ws.kts
+++ src/sol_01.ws.kts
@@ -1,2 +1,21 @@
-val l = listOf(126317, 64620, 139485, 77772, 104110, 103781, 62566, 76265, 122125)
+val l = listOf(14, 1969, 100756)
 l.sumBy{ Math.floorDiv(it, 3) - 2 }
+fun calcFuel(mass: Int): Int {
+    return Math.floorDiv(mass, 3) - 2
+}
+l.map { calcFuel(it) }
+fun calcRecFuel(mass: Int, acc: Int = 0): Int {
+    val newMass = calcFuel(mass)
+    return if (newMass > 0) {
+        calcRecFuel(newMass, acc + newMass)
+    } else acc
+}
+l.map { calcRecFuel(it) }
+fun calcListFuel(mass: Int, acc: List<Int> = emptyList()): List<Int> {
+    val newMass = calcFuel(mass)
+    return if (newMass <= 0) acc
+    else calcListFuel(newMass, acc + newMass)
+}
+val r1 = l.map { calcListFuel(it) }
+r1
+r1.map { it.sum() }
