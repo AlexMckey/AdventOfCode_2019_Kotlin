59a2c388829f7498e0655da0c7a905177dc7fa38
AlexMckey <mckey2002@yandex.ru>
2019-12-06 11:23:01 +0400

    Day 5 Part 1 Complete Move Computer class in library

---
+++ src/AOCLib/Computer.kt
@@ -0,0 +1,77 @@
+package AOCLib
+
+enum class ParameterMode {
+    Position, Immediate
+}
+
+private fun Char.toParameterMode(): ParameterMode {
+    return when (this) {
+        '1' -> ParameterMode.Immediate
+        else -> ParameterMode.Position
+    }
+}
+
+class Computer(program: IntArray = intArrayOf(99), private val input: Iterator<Int>) {
+    private var memory = program
+    operator fun get(address: Int) = memory[address]
+    operator fun set(address: Int, value: Int) {
+        memory[address] = value
+    }
+
+    private fun paramMode(opCode: Int, n: Int): ParameterMode =
+        (opCode / 100).toString()
+            .reversed().getOrElse(n - 1) { '0' }.toParameterMode()
+
+    private operator fun IntArray.get(ip: Int, n: Int): Int =
+        if (paramMode(memory[ip], n) == ParameterMode.Position) {
+            memory[memory[ip + n]]
+        } else {
+            memory[ip + n]
+        }
+
+    private operator fun IntArray.set(ip: Int, n: Int, value: Int) =
+        if (paramMode(memory[ip], n) == ParameterMode.Position) {
+            memory[memory[ip + n]] = value
+        } else {
+            memory[ip + n] = value
+        }
+
+    private var ip: Int = 0
+    private var step: Int = 0
+    private var isHalt: Boolean = false
+    var output: MutableCollection<Int> = mutableListOf()
+    val result: Int
+        get() = memory[0]
+
+    private fun doOneStep() {
+        when (memory[ip] % 100) {
+            1 -> {
+                memory[ip, 3] = memory[ip, 1] + memory[ip, 2]
+                ip += 4
+            }
+            2 -> {
+                memory[ip, 3] = memory[ip, 1] * memory[ip, 2]
+                ip += 4
+            }
+            3 -> {
+                memory[ip, 1] = input.next()
+                ip += 2
+            }
+            4 -> {
+                output.add(memory[ip, 1])
+                ip += 2
+            }
+            99 -> {
+                isHalt = true
+                ip += 1
+            }
+        }
+        step++
+    }
+
+    fun runProgram() {
+        step = 0
+        ip = 0
+        while (!isHalt) doOneStep()
+    }
+}
--- src/Sol05/Sol05.kt
+++ src/Sol05/Sol05.kt
@@ -1,12 +1,16 @@
 package Sol05
 
+import AOCLib.Computer
+import java.io.File
+
 fun main() {
     val fileName = "out/production/KtAOC2019/Sol05/input05.txt"
-    ///val inputStream = File(fileName).bufferedReader().readLine()
-    val inputStream = "1002,4,3,1,33"
+    val inputStream = File(fileName).bufferedReader().readLine()
+    //val inputStream = "1002,4,3,4,33"
+    //val inputStream = "3,0,4,0,99"
     val inputProgram = inputStream.split(',').map { it.toInt() }.toIntArray()
 
-    val computer = Computer(inputProgram.clone(), 1)
+    val computer = Computer(inputProgram.clone(), listOf(1).iterator())
     computer.runProgram()
 
     val res1 = computer.output.toString()
