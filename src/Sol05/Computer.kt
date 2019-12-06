5b44eca4a6cf0f5e45ce0bcc6c5aee782bd5d1e1
Alex Mc'key <mckey2002@yandex.ru>
2019-12-06 21:31:57 +0400

    Day 4 - Not work

--- src/Sol05/Computer.kt
+++ src/Sol05/Computer.kt
@@ -1,69 +1,39 @@
 package Sol05
 
-enum class ParameterMode {
-    Position, Immediate
-}
-
-data class Instruction(val length: Int, val action: (List<Int>) -> Unit)
-
-private fun Char.toParameterMode(): ParameterMode {
-    return when (this) {
-        '1' -> ParameterMode.Immediate
-        else -> ParameterMode.Position
-    }
-}
-
-class Computer(program: IntArray = intArrayOf(99), input: Int = 0) {
-    private var memory = intArrayOf(99)
-    fun get(address: Int) = memory[address]
-    fun set(address: Int, value: Int) {
+class Computer(program: IntArray, inputIterator: Iterator<Int>) {
+    private var ip = 0
+    private var step = 0
+    private val memory = program
+    operator fun get(address: Int) = memory[address]
+    operator fun set(address: Int, value: Int) {
         memory[address] = value
     }
 
-    private var ip: Int = 0
-    private var step: Int = 0
-    private var isHalt: Boolean = false
-    var output: List<Int> = emptyList()
-    private var io: Int = input
-        set(value: Int) {
-            field = value
-            output += field
-        }
-
+    private var isHalt = false
+    private val input = inputIterator
+    val output: MutableList<Int> = emptyList<Int>().toMutableList()
     val result: Int
         get() = memory[0]
 
-    init {
-        memory = program
-        step = 0
-        ip = 0
-        io = input
-        isHalt = false
-    }
-
-    private val instructions = hashMapOf(
-        1 to Instruction(3) { l -> memory[l[2]] = l[0] + l[1] },
-        2 to Instruction(3) { l -> memory[l[2]] = l[0] * l[1] },
-        3 to Instruction(1) { l -> memory[l[0]] = io },
-        4 to Instruction(1) { l -> io = memory[l[0]] },
-        99 to Instruction(0) { isHalt = true }
-    )
-
     private fun doOneStep(instruction: Int) {
         val opCode = instruction % 100
-        val opParamsCount = instructions[opCode]!!.length
+        val opLength = 4
         val modes = (instruction / 100).toString()
-            .padStart(opParamsCount, '0')
+            .padStart(opLength - 1, '0')
             .reversed()
-            .map { it.toParameterMode() }
-        val params = memory
-            .slice((ip + 1)..(ip + opParamsCount))
-            .zip(modes).map { (param, mode) ->
-                if (mode == ParameterMode.Immediate) param
-                else memory[param]
-            }
-        instructions[opCode]!!.action(params)
-        ip += opParamsCount + 1
+        val params = memory.sliceArray(ip + 1 until ip + opLength)
+        when (opCode) {
+            1 -> memory[params[2]] =
+                if (modes[0] == '1') params[0] else memory[params[0]] +
+                        if (modes[1] == '1') params[1] else memory[params[1]]
+            2 -> memory[params[2]] =
+                if (modes[0] == '1') params[0] else memory[params[0]] *
+                        if (modes[1] == '1') params[1] else memory[params[1]]
+            3 -> memory[params[2]] = input.next()
+            4 -> output.add(if (modes[0] == '1') params[0] else memory[params[0]])
+            99 -> isHalt = true
+        }
+        ip += opLength
     }
 
     fun runProgram() {
--- src/Sol05/Sol05.kt
+++ src/Sol05/Sol05.kt
@@ -3,13 +3,10 @@ package Sol05
 fun main() {
     val fileName = "out/production/KtAOC2019/Sol05/input05.txt"
     ///val inputStream = File(fileName).bufferedReader().readLine()
-    val inputStream = "1002,4,3,1,33"
+    val inputStream = "1002,4,3,4,33"
     val inputProgram = inputStream.split(',').map { it.toInt() }.toIntArray()
 
-    val computer = Computer(inputProgram.clone(), 1)
-    computer.runProgram()
-
-    val res1 = computer.output.toString()
-
-    println(res1) //
+    val computer = Computer(inputProgram.clone(), intArrayOf(1).iterator()).also(Computer::runProgram).also {
+        it.output.toString().also(::println)
+    } //
 }
