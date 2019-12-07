544c8d4bbccd70d9279361a8d754bfd2cf6c2463
Alex Mc'key <mckey2002@yandex.ru>
2019-12-07 15:58:11 +0400

    Adjust changes in Computer library

--- src/Sol05/Sol05.kt
+++ src/Sol05/Sol05.kt
@@ -14,14 +14,16 @@ fun main() {
     //val inputStream = "3,3,1107,-1,8,3,4,3,99"
     val inputProgram = inputStream.split(',').map { it.toInt() }.toIntArray()
 
-    val comp1 = Computer(inputProgram.clone(), listOf(1).iterator())
+    val comp1 = Computer(inputProgram.clone())
+    comp1.input.add(1)
     comp1.runProgram()
 
     val res1 = comp1.output.toString()
 
     println(res1) //7259358
 
-    val comp2 = Computer(inputProgram.clone(), listOf(5).iterator())
+    val comp2 = Computer(inputProgram.clone())
+    comp2.input.add(5)
     comp2.runProgram()
 
     val res2 = comp2.output.toString()
