c6dd0c8a8b93667ea34dc84499761f2de517c4d6
AlexMckey <mckey2002@yandex.ru>
2019-12-12 09:11:10 +0400

    Day 12 initial solution

---
+++ src/Sol12/Sol12.kt
@@ -0,0 +1,9 @@
+package Sol12
+
+import java.io.File
+
+fun main() {
+    val fileName = "out/production/KtAOC2019/Sol12/input12.txt"
+    val inputStream = File(fileName).bufferedReader().readLines()
+
+}
